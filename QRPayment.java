a72826bc1f385a630a846653a90af918b9b788f4
andronousss <raven24112006@gmail.com>
2025-10-09 17:56:06 +0500

    Update QRPayment.java

--- QRPayment.java
+++ QRPayment.java
@@ -1,6 +1,7 @@
 package SDP_assignment4;
 
-public class QRPayment {
+public class QRPayment implements PaymentMethod{
+    @Override
     public void pay(double amount) {
         System.out.println("Paid " + amount + "$" + " with QR Code");
     }
